c204a293a72efff93e87317c6feebe802a258e52
Marius Ungureanu <therzok@gmail.com>
2019-11-19 14:44:47 -0500

    Add complete sample and test

--- analyzer/Class1.cs
+++ analyzer/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace analyzer
@@ -9,14 +10,20 @@ namespace analyzer
     // https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/tutorials/how-to-write-csharp-analyzer-code-fix
     // https://roslyn-analyzers.readthedocs.io/en/latest/create-nuget-package.html
 
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class MyAnalyzer : DiagnosticAnalyzer
     {
         // Just a dummy here.
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+            new DiagnosticDescriptor("TEST001", "msg", "msg", "TEST", DiagnosticSeverity.Warning, true)
+        );
 
         public override void Initialize(AnalysisContext context)
         {
-            throw new NotImplementedException();
+            context.RegisterSyntaxNodeAction(nodeContext =>
+            {
+                nodeContext.ReportDiagnostic(Diagnostic.Create(SupportedDiagnostics[0], nodeContext.Node.GetLocation()));
+            }, ImmutableArray.Create(SyntaxKind.ClassDeclaration));
         }
     }
 }
